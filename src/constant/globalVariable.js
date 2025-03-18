9567df6bb57ab5f6a90641e4f91d3ec88a2c5b05
jongdae <asdfg9377@naver.com>
2025-03-19 00:50:36 +0900

    feat: global variable

---
+++ src/constant/globalVariable.js
@@ -0,0 +1,17 @@
+/**
+ * 프로젝트에서 사용할 전역 변수들의 모음입니다.
+ */
+
+export const globalVariable = {
+  // game 진행 상태
+  isGameRunning: false,
+
+  // 현재 game의 target number
+  targetNumber: null,
+
+  // strike count
+  strikeCount: 0,
+
+  // ball count
+  ballCount: 0,
+};
