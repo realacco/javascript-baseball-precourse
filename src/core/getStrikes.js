909276ea8658df40a579b83f883a2fdedb46040a
jongdae <asdfg9377@naver.com>
2025-03-19 01:18:46 +0900

    refactor: 검증과정 제거

--- src/core/getStrikes.js
+++ src/core/getStrikes.js
@@ -4,18 +4,11 @@
  * @param {number[]} targetNumbers
  * @returns {number}
  */
-export const getStrikes = (userNumbers, targetNumbers) => {
-  // 입력값 검증
-  if (!Array.isArray(userNumbers) || !Array.isArray(targetNumbers)) {
-    throw new Error('입력값이 배열이 아닙니다.');
-  }
-
-  // 숫자로 변환하여 비교
-  return userNumbers.reduce((acc, userNumber, index) => {
+export const getStrikes = (userNumbers, targetNumbers) =>
+  userNumbers.reduce((acc, userNumber, index) => {
     const user = Number(userNumber);
     const target = Number(targetNumbers[index]);
-    
+
     if (user === target) return acc + 1;
     return acc;
   }, 0);
-};
