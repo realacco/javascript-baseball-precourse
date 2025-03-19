66ccf9ea9e67901737a3e64d2d238d79f53d2806
jongdae <asdfg9377@naver.com>
2025-03-19 19:55:05 +0900

    refactor: parameter type number[] 배열로 변경

--- src/core/getStrikes.js
+++ src/core/getStrikes.js
@@ -4,11 +4,12 @@
  * @param {number[]} targetNumbers
  * @returns {number}
  */
-export const getStrikes = (userNumbers, targetNumbers) =>
-  userNumbers.reduce((acc, userNumber, index) => {
-    const user = Number(userNumber);
-    const target = Number(targetNumbers[index]);
+export const getStrikes = (userNumbers, targetNumbers) => {
+  return userNumbers.reduce((acc, userNumber, index) => {
+    const user = userNumber;
+    const target = targetNumbers[index];
 
     if (user === target) return acc + 1;
     return acc;
   }, 0);
+};
